a5597d52df16a1f3f2af33baefa9d131d4ed8992
TOMMY STANLEY VEGA SERVELLON <tommysvs@unitec.edu>
2020-09-07 12:09:43 -0600

    Add project files.

---
+++ Matriz.cpp
@@ -0,0 +1,8 @@
+#include "Matriz.h"
+
+using namespace Matrices;
+
+void main() {
+	Application::EnableVisualStyles();
+	Application::Run(gcnew Matriz());
+}
---
+++ Matriz.h
@@ -0,0 +1,88 @@
+#pragma once
+
+namespace Matrices {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for Matriz
+	/// </summary>
+	public ref class Matriz : public System::Windows::Forms::Form
+	{
+	public:
+		Matriz(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Matriz()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Matriz::typeid));
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Montserrat", 21.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->ForeColor = System::Drawing::Color::Coral;
+			this->label1->Location = System::Drawing::Point(402, 32);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(142, 38);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Matrices";
+			this->label1->Click += gcnew System::EventHandler(this, &Matriz::label1_Click);
+			// 
+			// Matriz
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::SeaShell;
+			this->ClientSize = System::Drawing::Size(974, 562);
+			this->Controls->Add(this->label1);
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
+			this->Name = L"Matriz";
+			this->Text = L"Matrices";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	};
+}
